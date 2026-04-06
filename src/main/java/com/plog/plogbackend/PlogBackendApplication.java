0942035507cbeddd0d295552e54f61cf3381ec4d
LeeDoHyun <wonehgus1234@naver.com>
2026-04-06 21:07:46 +0900

    Add files via upload
    init

---
+++ src/main/java/com/plog/plogbackend/PlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.plog.plogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plog/plogbackend/PlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plog.plogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
