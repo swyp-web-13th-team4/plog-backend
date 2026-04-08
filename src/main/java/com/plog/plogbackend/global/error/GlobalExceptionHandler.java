d8a844be42a18858e9faa28a6c3aa35835b6d35d
leedohyun <wonehgus1234@naver.com>
2026-04-08 18:14:18 +0900

    feat: 글로벌 에러 핸들러 기본 클래스 추가

---
+++ src/main/java/com/plog/plogbackend/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.plog.plogbackend.global.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class) //예시 코드 개발 시작시 삭제
+    public ResponseEntity<Object> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
