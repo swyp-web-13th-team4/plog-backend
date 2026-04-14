6e2b1d679e04815a6ca189056845b0e01c4ba525
2giseok <rltjr147@gmail.com>
2026-04-14 15:47:56 +0900

    [feat] Post 도메인 추가

---
+++ src/main/java/com/plog/plogbackend/domain/post/entity/PostImage.java
@@ -0,0 +1,16 @@
+package com.plog.plogbackend.domain.post.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class PostImage {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
---
+++ src/main/java/com/plog/plogbackend/domain/post/entity/PublicScope.java
@@ -0,0 +1,5 @@
+package com.plog.plogbackend.domain.post.entity;
+
+public enum PublicScope {
+    PUBLIC, PRIVATE
+}
