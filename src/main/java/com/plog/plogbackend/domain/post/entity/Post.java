0aff1279536528188350bfa3d95037bd5f08cb80
이기석 <rltjr147@gmail.com>
2026-04-15 16:14:20 +0900

    feat/ post 도메인 추가 및 중복 제거
    * [feat] Post 도메인 추가
    
    * [feat] Post 도메인 기본 구조 추가
    
    * [feat] Post 도메인 기본 구조 추가
    
    * [feat] Post 도메인 기본 구조 추가
    
    * [refactor] 병합 후 중복 파일 제거 및 구조 정리
    
    * [refactor] 병합 후 중복 파일 제거 및 구조 정리

---
+++ src/main/java/com/plog/plogbackend/domain/post/entity/Post.java
@@ -0,0 +1,54 @@
+package com.plog.plogbackend.domain.post.entity;
+
+import com.plog.plogbackend.domain.Member.Member;
+import com.plog.plogbackend.domain.place.entity.Place;
+import com.plog.plogbackend.global.common.entity.BaseTimeStatusEntity;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Post extends BaseTimeStatusEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+
+  private String contents;
+
+  private LocalDateTime startedAt;
+  private LocalDateTime endedAt;
+
+  private LocalDate studyDate;
+
+  private Integer studyTime;
+
+  private Integer focus;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private PublicScope scope;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "place_id")
+  private Place place;
+
+  @OneToMany(mappedBy = "post")
+  private List<PostImage> images = new ArrayList<>();
+
+  @OneToMany(mappedBy = "post")
+  private List<PostTag> tags = new ArrayList<>();
+
+  @OneToMany(mappedBy = "post")
+  private List<PostCategory> categories = new ArrayList<>();
+}
