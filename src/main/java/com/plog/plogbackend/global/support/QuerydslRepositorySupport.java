b8a66a87150b7c14ba9653a04d93b594cdff34e9
mkkim21 <dgd5980@naver.com>
2026-04-15 17:05:08 +0900

    feature[#30]: expression select 추가

--- src/main/java/com/plog/plogbackend/global/support/QuerydslRepositorySupport.java
+++ src/main/java/com/plog/plogbackend/global/support/QuerydslRepositorySupport.java
@@ -1,6 +1,7 @@
 package com.plog.plogbackend.global.support;
 
 import com.querydsl.core.types.EntityPath;
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -13,54 +14,56 @@ import org.springframework.data.querydsl.SimpleEntityPathResolver;
 
 public abstract class QuerydslRepositorySupport {
 
-    private Querydsl querydsl;
-    private EntityManager entitymanager;
-    private JPAQueryFactory jpaQueryFactory;
+  private Querydsl querydsl;
+  private EntityManager entityManager;
+  private JPAQueryFactory jpaQueryFactory;
 
-    private final Class<?> entityClass;
+  private final Class<?> entityClass;
 
+  protected QuerydslRepositorySupport(Class<?> entityClass) {
+    this.entityClass = entityClass;
+  }
 
-    protected QuerydslRepositorySupport(Class<?> entityClass) {
-        this.entityClass = entityClass;
-    }
+  @PersistenceContext
+  public void setEntityManager(EntityManager entityManager) {
+    this.entityManager = entityManager;
 
-    @PersistenceContext
-    public void setEntitymanager(EntityManager entitymanager) {
-        this.entitymanager = entitymanager;
+    var entityInformation =
+        JpaEntityInformationSupport.getEntityInformation(entityClass, entityManager);
 
-        var entityInformation =
-                JpaEntityInformationSupport.getEntityInformation(entityClass, entitymanager);
+    EntityPath<?> path =
+        SimpleEntityPathResolver.INSTANCE.createPath(entityInformation.getJavaType());
+    PathBuilder<?> builder = new PathBuilder<>(path.getType(), path.getMetadata());
 
-        EntityPath<?> path =
-                SimpleEntityPathResolver.INSTANCE.createPath(entityInformation.getJavaType());
-        PathBuilder<?> builder = new PathBuilder<>(path.getType(), path.getMetadata());
+    this.querydsl = new Querydsl(entityManager, builder);
+    this.jpaQueryFactory = new JPAQueryFactory(entityManager);
+  }
 
-        this.querydsl = new Querydsl(entitymanager, builder);
-        this.jpaQueryFactory = new JPAQueryFactory(entitymanager);
-    }
+  protected <T> JPAQuery<T> select(EntityPath<T> select) {
+    return jpaQueryFactory.select(select);
+  }
 
-    protected <T> JPAQuery<T> select(EntityPath<T> select) {
-        return jpaQueryFactory.select(select);
-    }
+  protected <T> JPAQuery<T> select(Expression<T> select) {
+    return jpaQueryFactory.select(select);
+  }
 
-    protected <T> JPAQuery<T> selectFrom(EntityPath<T> from) {
-        return jpaQueryFactory.selectFrom(from);
-    }
+  protected <T> JPAQuery<T> selectFrom(EntityPath<T> from) {
+    return jpaQueryFactory.selectFrom(from);
+  }
 
-    protected <T>JPAQuery<Integer> selectOne(){
-        return jpaQueryFactory.selectOne();
-    }
+  protected JPAQuery<Integer> selectOne() {
+    return jpaQueryFactory.selectOne();
+  }
 
-    protected <T>JPAUpdateClause update(EntityPath<T> from) {
-        return jpaQueryFactory.update(from);
-    }
+  protected <T> JPAUpdateClause update(EntityPath<T> from) {
+    return jpaQueryFactory.update(from);
+  }
 
-    public void flush() {
-        entitymanager.flush();
-    }
+  public void flush() {
+    entityManager.flush();
+  }
 
-    public void clear() {
-        entitymanager.clear();
-    }
-
+  public void clear() {
+    entityManager.clear();
+  }
 }
