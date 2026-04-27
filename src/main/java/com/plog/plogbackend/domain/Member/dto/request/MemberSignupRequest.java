d9010275c64af7d6520e9ee36a16533f4277d40c
leedohyun <wonehgus1234@naver.com>
2026-04-27 09:10:05 +0900

    chore:소개글 선택사항으로 변경

--- src/main/java/com/plog/plogbackend/domain/Member/dto/request/MemberSignupRequest.java
+++ src/main/java/com/plog/plogbackend/domain/Member/dto/request/MemberSignupRequest.java
@@ -10,8 +10,6 @@ public record MemberSignupRequest(
         @NotBlank(message = "필수 입력 항목입니다.")
         @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하여야 합니다.")
         String nickname,
-    @Schema(description = "소개글 (필수, 최대 100자)")
-        @NotBlank(message = "필수 입력 항목입니다.")
-        @Size(max = 100, message = "소개글은 최대 100자까지 가능합니다.")
+    @Schema(description = "소개글 (선택, 최대 100자)") @Size(max = 100, message = "소개글은 최대 100자까지 가능합니다.")
         String introduction,
     @Schema(description = "약관 동의 여부") Map<Long, Boolean> termsAgreements) {}
--- src/main/java/com/plog/plogbackend/domain/Member/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/plog/plogbackend/domain/Member/dto/request/UpdateProfileRequest.java
@@ -15,8 +15,7 @@ public record UpdateProfileRequest(
         @NotBlank(message = "필수 입력 항목입니다.")
         @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하여야 합니다.")
         String nickname,
-    @Schema(description = "소개글 (필수, 최대 100자)", example = "안녕하세요!")
-        @NotBlank(message = "필수 입력 항목입니다.")
+    @Schema(description = "소개글 (선택, 최대 100자)", example = "안녕하세요!")
         @Size(max = 100, message = "소개글은 최대 100자까지 가능합니다.")
         String introduction
 
