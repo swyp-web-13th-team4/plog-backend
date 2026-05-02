42773cbc5fb0c616afe5b28627722f34a2cf6bcc
류정균 <prank01@naver.com>
2026-05-02 18:54:24 +0900

    feat:Tag 엔티티 추가

---
+++ src/main/java/com/plog/plogbackend/domain/tag/entity/Tag.java
@@ -0,0 +1,27 @@
+package com.plog.plogbackend.domain.tag.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+  private String name;
+
+  public static Tag of(String name) {
+      Tag tag = new Tag();
+      tag.name = name;
+      return tag;
+  }
+
+}
