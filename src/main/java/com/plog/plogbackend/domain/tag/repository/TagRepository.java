4e15696be9ec6ab48cc2f0f435320c589b331959
류정균 <prank01@naver.com>
2026-05-02 19:16:44 +0900

    feat: tagRepository 추가

---
+++ src/main/java/com/plog/plogbackend/domain/tag/repository/TagRepository.java
@@ -0,0 +1,12 @@
+package com.plog.plogbackend.domain.tag.repository;
+
+import com.plog.plogbackend.domain.tag.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    List<Tag> findByName(String name);
+    List<Tag> findByNameIn(List<String> names);
+}
