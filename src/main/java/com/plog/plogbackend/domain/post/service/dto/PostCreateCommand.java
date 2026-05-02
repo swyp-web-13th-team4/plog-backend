fe3cdfa1913c81fa40219ce34c079c332788d254
류정균 <prank01@naver.com>
2026-05-02 21:42:23 +0900

    feat: PostService, Service 엔드포인트 dto 추가

---
+++ src/main/java/com/plog/plogbackend/domain/post/service/dto/PostCreateCommand.java
@@ -0,0 +1,22 @@
+package com.plog.plogbackend.domain.post.service.dto;
+
+import com.plog.plogbackend.domain.post.entity.PublicScope;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record PostCreateCommand(
+    String title,
+    String contents,
+    LocalDateTime startedAt,
+    LocalDateTime endedAt,
+    LocalDate studyDate,
+    Integer studyTime,
+    Integer focus,
+    PublicScope scope,
+    String placeName,
+    List<String> tagNames,
+    List<String> categoryNames,
+    UUID memberKey) {}
