31509e24eb84c37f829f52fbd4442ab5965921be
류정균 <prank01@naver.com>
2026-05-03 14:01:42 +0900

    PlaceTag Enum 추가 및 Tag 엔티티 PlaceTag enum 추가, Tag 레포 변경

--- src/main/java/com/plog/plogbackend/domain/tag/Tag.java
+++ src/main/java/com/plog/plogbackend/domain/tag/Tag.java
@@ -1,18 +1,19 @@
 package com.plog.plogbackend.domain.tag;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.plog.plogbackend.domain.tag.enums.PlaceTag;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Tag {
 
+    @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Id
   private Long id;
 
-  private String name;
+    @Enumerated(EnumType.STRING)
+  private PlaceTag placeTag;
 }
--- src/main/java/com/plog/plogbackend/domain/tag/repository/TagRepository.java
+++ src/main/java/com/plog/plogbackend/domain/tag/repository/TagRepository.java
@@ -2,11 +2,13 @@ package com.plog.plogbackend.domain.tag.repository;
 
 import com.plog.plogbackend.domain.tag.Tag;
 import java.util.List;
+
+import com.plog.plogbackend.domain.tag.enums.PlaceTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-  List<Tag> findByName(String name);
+  List<Tag> findByPlaceTag(PlaceTag placeTag);
 
-  List<Tag> findByNameIn(List<String> names);
+  List<Tag> findByPlaceTagIn(List<PlaceTag> placeTag);
 }
