186a5feaccfef094b7283e4dfca4d74c7a00e850
Ryu JungKyun <164990532+ryujungkyun@users.noreply.github.com>
2026-05-04 01:20:53 +0900

    Feature/post Enum 타입의 api 안전성 향상 및 TimePickeRequest Dto 생성  (#69)
    * refactor: PlaceTag 견고성 향상
    
    * feat:TimePickerRequest 추가
    
    * refactor: PublicScope 견고성 향상
    
    * feat:style 적용

---
+++ src/main/java/com/plog/plogbackend/domain/post/controller/dto/request/post/TimePickerRequest.java
@@ -0,0 +1,12 @@
+package com.plog.plogbackend.domain.post.controller.dto.request.post;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+/**
+ * 타임 피커는 {hour: 12, minute: 30}을 전송합니다 이에 맞게 TimePicker용 DTO를 만들었습니다
+ *
+ * @param hour
+ * @param minute
+ */
+public record TimePickerRequest(@Min(0) @Max(23) Integer hour, @Min(0) @Max(59) Integer minute) {}
