8880090ae1806f2556760a880606a4a73ecea752
2giseok <rltjr147@gmail.com>
2026-05-09 11:14:51 +0900

    [refactor] 시간 포맷 수정

--- src/main/java/com/plog/plogbackend/domain/post/controller/dto/request/FeedFindRequest.java
+++ src/main/java/com/plog/plogbackend/domain/post/controller/dto/request/FeedFindRequest.java
@@ -4,4 +4,4 @@ import java.time.LocalDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public record FeedFindRequest(
-    long lastPostId, @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createAt) {}
+    long lastPostId, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime createAt) {}
