8b461fab4d1bc3e7f1a976f54fe9bbdc1003d31f
2giseok <rltjr147@gmail.com>
2026-05-09 11:33:20 +0900

    [refactor] 시간 포맷 수정

--- src/main/java/com/plog/plogbackend/domain/post/controller/dto/response/FeedDetailResponse.java
+++ src/main/java/com/plog/plogbackend/domain/post/controller/dto/response/FeedDetailResponse.java
@@ -1,5 +1,6 @@
 package com.plog.plogbackend.domain.post.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.plog.plogbackend.domain.post.entity.Post;
 import com.plog.plogbackend.domain.post.entity.PostImage;
 import com.plog.plogbackend.domain.tag.enums.PlaceTag;
@@ -12,6 +13,7 @@ public record FeedDetailResponse(
     UUID memerKey,
     String name,
     String profileImage,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     LocalDateTime createAt,
     List<String> postImages,
     Long likes,
