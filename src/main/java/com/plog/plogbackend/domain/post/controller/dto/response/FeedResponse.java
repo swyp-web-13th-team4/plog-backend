389d95c4b863d7293160c689de9ab4c9dcec4294
2giseok <rltjr147@gmail.com>
2026-05-09 11:14:51 +0900

    [refactor] 시간 포맷 수정

--- src/main/java/com/plog/plogbackend/domain/post/controller/dto/response/FeedResponse.java
+++ src/main/java/com/plog/plogbackend/domain/post/controller/dto/response/FeedResponse.java
@@ -1,7 +1,12 @@
 package com.plog.plogbackend.domain.post.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record FeedResponse(
-    List<FeedFindResponse> feedFindResponses, Long lastPostId, LocalDateTime createdAt) {}
+    List<FeedFindResponse> feedFindResponses, Long lastPostId,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
+    LocalDateTime createdAt) {}
