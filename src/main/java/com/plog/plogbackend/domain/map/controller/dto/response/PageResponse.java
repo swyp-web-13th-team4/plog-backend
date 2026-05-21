e8ca3f72d32e31f1a7f01617a69211dbe6ac1ca5
mkkim21 <dgd5980@naver.com>
2026-05-21 10:44:50 +0900

    refactor[#150]: 페이징 인프라 관련 클래스 리팩토링

--- src/main/java/com/plog/plogbackend/domain/map/controller/dto/response/PageResponse.java
+++ src/main/java/com/plog/plogbackend/domain/map/controller/dto/response/PageResponse.java
@@ -3,8 +3,8 @@ package com.plog.plogbackend.domain.map.controller.dto.response;
 import com.plog.plogbackend.global.support.paging.Slice;
 import java.util.List;
 
-public record PageResponse<T>(List<T> content, boolean hasNext) {
+public record PageResponse<T>(List<T> content, boolean hasNext, String nextCursor) {
   public static <T> PageResponse<T> of(Slice<T> slice) {
-    return new PageResponse<>(slice.getContent(), slice.isHasNext());
+    return new PageResponse<>(slice.getContent(), slice.isHasNext(), slice.getNextCursor());
   }
 }
