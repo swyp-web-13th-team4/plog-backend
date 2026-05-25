e84de5fe55ba2b90784402134e7be2de82500403
류정균 <prank01@naver.com>
2026-05-26 01:11:26 +0900

    fix:PlaceReview 참조 무결성 제약으로 인해 Post Delte가 안되는 문제 수정

--- src/main/java/com/plog/plogbackend/domain/review/repository/PlaceReviewRepository.java
+++ src/main/java/com/plog/plogbackend/domain/review/repository/PlaceReviewRepository.java
@@ -4,6 +4,8 @@ import com.plog.plogbackend.domain.review.entity.PlaceReview;
 import com.plog.plogbackend.global.common.Enum.EntityStatus;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long> {
 
@@ -12,4 +14,20 @@ public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long> 
   Optional<PlaceReview> findByPostId(Long postId);
 
   Optional<PlaceReview> findByIdAndStatus(Long id, EntityStatus status);
+
+  @Modifying
+  @Query(
+      """
+      DELETE FROM PlaceReviewEnvironment environment
+      WHERE environment.placeReview.id IN (
+        SELECT review.id
+        FROM PlaceReview review
+        WHERE review.post.id = :postId
+      )
+      """)
+  void deleteEnvironmentsByPostId(Long postId);
+
+  @Modifying
+  @Query("DELETE FROM PlaceReview review WHERE review.post.id = :postId")
+  void deleteByPostId(Long postId);
 }
