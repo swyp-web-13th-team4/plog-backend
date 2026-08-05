28a41f65fca97ba82af2489b79fd13f88c0e5a09
RyuJungKyun <prank01@naver.com>
2026-08-05 10:25:43 +0900

    feat: PlaceReviewStatistics 쿼리에서 삭제된 리뷰 필터링 및 테스트 추가

--- src/main/java/com/plog/plogbackend/domain/review/repository/PlaceReviewStatisticsRepository.java
+++ src/main/java/com/plog/plogbackend/domain/review/repository/PlaceReviewStatisticsRepository.java
@@ -6,6 +6,7 @@ import static com.plog.plogbackend.domain.review.entity.QPlaceReviewEnvironment.
 
 import com.plog.plogbackend.domain.review.repository.dto.PlaceReviewEnvironmentCount;
 import com.plog.plogbackend.domain.review.repository.dto.PlaceReviewRatingSummary;
+import com.plog.plogbackend.global.common.Enum.EntityStatus;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -25,7 +26,7 @@ public class PlaceReviewStatisticsRepository {
                 PlaceReviewRatingSummary.class, placeReview.id.count(), placeReview.rating.avg()))
         .from(placeReview)
         .join(placeReview.post, post)
-        .where(post.place.id.eq(placeId))
+        .where(post.place.id.eq(placeId), placeReview.status.eq(EntityStatus.ACTIVE))
         .fetchOne();
   }
 
@@ -40,7 +41,7 @@ public class PlaceReviewStatisticsRepository {
         .from(placeReviewEnvironment)
         .join(placeReviewEnvironment.placeReview, placeReview)
         .join(placeReview.post, post)
-        .where(post.place.id.eq(placeId))
+        .where(post.place.id.eq(placeId), placeReview.status.eq(EntityStatus.ACTIVE))
         .groupBy(placeReviewEnvironment.name, placeReviewEnvironment.score)
         .fetch();
   }
